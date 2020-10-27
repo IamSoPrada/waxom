7189eb3e194da1688a2a00870aa9f182a896b5ad
IamSoPrada <9999goldcoin@gmail.com>
2020-10-28 00:17:26 +0300

    Added gsap

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
 
+document.addEventListener("DOMContentLoaded", () => {
+    
+
+    gsap.registerPlugin(ScrollToPlugin, ScrollTrigger)
 
 function openBurger(){
     let burger = document.querySelector(".header__burger")
@@ -15,4 +18,53 @@ function openBurger(){
 }
 openBurger()
 
+
+gsap.from(".promo__title_main", {
+    scrollTrigger: {
+        trigger: ".promo__title_main",
+        toggleActions: "restart"
+    }, 
+    y: -300,
+    duration: 1,
+    opacity: 0
+  });
+
+gsap.from(".features__item", {
+    scrollTrigger: ".features__item",
+    duration: 1,
+    scale: 0.5, 
+    opacity: 0, 
+    delay: 0.5, 
+    stagger: 0.2,
+    ease: "power2.out", 
+    force3D: true
+  });
+
+gsap.from(".mobile__img", {
+    scrollTrigger: ".mobile__img", 
+    x: -500,
+    duration: 2,
+    opacity: 0
+  });
+
+gsap.from(".mobile__descr", {
+    scrollTrigger: ".mobile__descr", 
+    x: 500,
+    duration: 2,
+    opacity: 0
+  });
+
+gsap.from(".projects__card-item", {
+    scrollTrigger: {
+        trigger: ".projects__card-item",
+        toggleActions: "restart"
+    },
+    duration: 0.9,
+    scale: 0.5, 
+    opacity: 0, 
+    delay: 0.3, 
+    stagger: 0.1,
+    ease: "power3.inOut", 
+    force3D: true
+  });
 })
